91fc6c37033431050c370532c8a10d4592b1117e
Raphael Alberto Dayan <raphael-dayan4@hotmail.com>
2024-08-06 17:04:02 +0300

    first commit - adding datasets, customer_landing.png, customer_landing.sql, accelerometer_landing.png, accelerometer_landing.sql

---
+++ accelerometer_landing.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS accelerometer_landing (
+    user STRING,
+    timestamp BIGINT,
+    x FLOAT,
+    y FLOAT,
+    z FLOAT,
+);
---
+++ customer_landing.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS customer_landing (
+    customerName STRING,
+    email STRING,
+    phone STRING,
+    birthDay STRING,
+    serialNumber STRING,
+    registrationDate BIGINT,
+    lastupdateDate BIGINT,
+    shareWithResearchAsOfDate BIGINT,
+    shareWithPublicAsOfDate BIGINT,
+    shareWithFriendsAsOfDate BIGINT
+);
